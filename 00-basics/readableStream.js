c35b3457cfba361b9b1c3247f9356d1cae1fda07
Maksim Ivanov <satansdeer@gmail.com>
2019-12-27 15:13:21 +0100

    webinar-intro-for-js

--- 00-basics/readableStream.js
+++
@@ -1,8 +0,0 @@
-const { Readable } = require('stream');
-
-const rs = new Readable();
-rs.push('ping ');
-rs.push('pong\n');
-rs.push(null);
-
-rs.pipe(process.stdout);
--- 00-basics/serverStream.js
+++
@@ -1,8 +0,0 @@
-const http = require('http')
-const fs = require('fs')
-
-const server = http.createServer((req, res) => {
-  const stream = fs.createReadStream(__dirname + '/data.txt')
-  stream.pipe(res)
-})
-server.listen(3000)
